1111ff7cca7375b9a33c486352a845f3127ca3fc
stiansenandreas <stiansenandreas@gmail.com>
2019-10-15 20:58:33 +0200

    .

--- powerline-theme.py
+++ powerline-theme.py
@@ -6,13 +6,14 @@ class Color(DefaultColor):
     USERNAME_BG = 12
     HOSTNAME_FG = 15
     HOSTNAME_BG = 10
-
+    
     HOME_SPECIAL_DISPLAY = False
-    PATH_BG = 13
+    PATH_BG = 11
     PATH_FG = 15
-    CWD_BG = 3
     CWD_FG = 15
-    SEPARATOR_FG = 7
+    SEPARATOR_FG = 15
 
-    CMD_PASSED_BG = 11
+    CMD_PASSED_BG = 13
     CMD_PASSED_FG = 15
+    CMD_FAILED_BG = 13
+    CMD_FAILED_FG = 15
